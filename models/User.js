946b6554f449a1261aa6010bb6cc96f403bf53b5
Cristian David Pinto Penaloza <cristiandavid.pinto@globant.com>
2020-05-21 09:32:33 -0500

    Added reference

--- models/User.js
+++ models/User.js
@@ -4,19 +4,22 @@ const userSchema = mongoose.Schema({
   name: {
     type: String,
     required: true,
-    trim: true,
   },
   email: {
     type: String,
     required: true,
     unique: true,
-    trim: true,
   },
   password: {
     type: String,
     required: true,
-    trim: true,
+    minlength: 6,
   },
+  image: {
+    type: String,
+    required: true,
+  },
+  places: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Place' }],
 });
 
 module.exports = mongoose.model('User', userSchema);
