9a50f2a625d47bbe5b7d7854a12ae02fffea1c01
Cristian David Pinto Penaloza <cristiandavid.pinto@globant.com>
2020-05-21 08:06:18 -0500

    Added error handlers

--- controllers/places.js
+++ controllers/places.js
@@ -1,9 +1,41 @@
 const Place = require('../models/Place');
+const { validationResult } = require('express-validator');
 const placeCtrl = {};
 
 placeCtrl.createPlace = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(422).json({
+      success: false,
+      message: 'Invalid inputs passed, please check your data',
+    });
+  }
+
   const { title, description, address } = req.body;
   const { userLoggedId } = req;
+
+  try {
+    const uniqueAddress = await Place.find({ address: address });
+    if (uniqueAddress.length) {
+      return res.status(422).json({
+        success: false,
+        message: 'A place with same address already exist',
+      });
+    }
+
+    const uniqueTitle = await Place.find({ title: title });
+    if (uniqueTitle.length) {
+      return res.status(422).json({
+        success: false,
+        message: 'A place with same title already exist',
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Fetching place failed, please try again.',
+    });
+  }
 
   const place = new Place({
     title,
@@ -27,7 +59,7 @@ placeCtrl.createPlace = async (req, res) => {
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: error,
+      message: 'Creating place failed, please try again.',
     });
   }
 };
@@ -45,7 +77,14 @@ placeCtrl.getPlaceById = async (req, res) => {
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: error,
+      message: 'Something went wrong, could not find a place.',
+    });
+  }
+
+  if (!place) {
+    res.status(404).json({
+      success: false,
+      message: 'Could not find place for the provided id.',
     });
   }
 };
@@ -63,20 +102,42 @@ placeCtrl.getUserPlaces = async (req, res) => {
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: error,
+      message: 'Fetching places failed, please try again later.',
+    });
+  }
+
+  if (!userPlaces.length) {
+    res.status(404).json({
+      success: false,
+      message: 'Could not find places for the provided user id.',
     });
   }
 };
 
 placeCtrl.deletePlace = async (req, res) => {
   const { id } = req.params;
+  const { userLoggedId } = req;
 
   try {
-    await Place.findById(id);
+    const place = await Place.findById(id);
+
+    if (!place) {
+      res.status(404).json({
+        success: false,
+        message: 'Could not find place for this id.',
+      });
+    }
+
+    if (place.creator !== userLoggedId) {
+      res.status(401).json({
+        success: false,
+        message: 'You are not allowed to delete this place.',
+      });
+    }
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: 'The place id does not exist',
+      message: 'Something went wrong, could not delete place.',
     });
   }
 
@@ -89,7 +150,7 @@ placeCtrl.deletePlace = async (req, res) => {
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: error,
+      message: 'Something went wrong, could not delete place.',
     });
   }
 };
