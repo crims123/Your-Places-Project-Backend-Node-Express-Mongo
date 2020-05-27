8b2409c3463907b1bc02954d4d681f672bfb87cf
Cristian David Pinto Penaloza <cristiandavid.pinto@globant.com>
2020-05-27 16:40:59 -0500

    Added map box token as env variable

--- utils/location.js
+++ utils/location.js
@@ -4,8 +4,7 @@ const getCoordsForAddress = async (address) => {
   const url = 'https://api.mapbox.com/geocoding/v5';
   const endpoint = 'mapbox.places';
   const searchText = encodeURIComponent(address);
-  const MAPBOX_ACCESS_TOKEN =
-    'pk.eyJ1IjoiY3JpbXMxMjMiLCJhIjoiY2thcHJ4dWRiMDJ1cDJ4bGd2dnJyd3JqMSJ9.SaqcnIsXanWsi3j2D7U8pw';
+  const MAPBOX_ACCESS_TOKEN =process.env.MAPBOX_ACCESS_TOKEN;
 
   const response = await axios(
     `${url}/${endpoint}/${searchText}.json/?access_token=${MAPBOX_ACCESS_TOKEN}`
